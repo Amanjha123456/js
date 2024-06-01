ff664d1a7b63d099d1c768b9780e32785855f1fb
Aman Kumar <121496039+Amanjha123456@users.noreply.github.com>
2024-06-01 16:39:35 +0000

    comparison operator

---
+++ .devcontainer/Basics/comparison.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 >= 2);
+console.log(2 !=1);
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//comparison convert null to a number,treating it as 0.That's why 3rd is true and else all are false
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+console.log(undefined != 0);
+
+// === values ko strictly check karta hai,keval data hi nahi balki datatypes ko bhi check karta hai
+console.log("2" === 2);
+
--- .devcontainer/Basics/conversionOperation.js
+++ .devcontainer/Basics/conversionOperation.js
@@ -30,3 +30,40 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ************************* Operations ***********************************
+let value = 3
+let negvalue = -value
+//console.log(value)
+//console.log(negvalue)
+
+console.log(2+2);
+console.log(2-3);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "aman"
+let str2 = " jha"
+str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //string first hai to sabko string banao
+console.log(1 + 2 + "2"); //string last hai to pehle jo hai uska kaam kar lo 
+console.log( (3 + 4) * 5 % 3);
+console.log(+true);
+//console.log(true+); // nahi hoga kyuki last me plus ka koi matlab nahi hai
+console.log(+"");
+x = ""
+console.log(x);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
