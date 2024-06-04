978b30eb4b7eb56a7be90bf948a94f2f74f01589
Aman Kumar <121496039+Amanjha123456@users.noreply.github.com>
2024-06-04 22:52:18 +0000

    object 3

---
+++ .devcontainer/Basics/object3.js
@@ -0,0 +1,26 @@
+//destructuring
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Aman Jha "
+}
+
+//course.courseInstructor
+const {courseInstructor: instructor} = course
+//console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "aman ")
+// {
+//     "name": "aman",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+[
+    {},
+    {},
+    {}
+]
