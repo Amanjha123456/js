487c4a2735d1dc1300af845016a4552b3ddee6ca
Aman Kumar <121496039+Amanjha123456@users.noreply.github.com>
2024-06-10 17:55:00 +0000

    while loop

---
+++ .devcontainer/Basics/whileloop.js
@@ -0,0 +1,26 @@
+
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2
+
+    
+}
+let arr = 0
+let myarray = ['flash','batman','superman']
+while (arr < myarray.length) {
+    //console.log(`value of array index is ${myarray[arr]}`);
+    arr = arr + 1
+}
+let score = 11
+
+ do  {
+    console.log(`score is ${score}`);
+    score++
+ }
+ while (score <=10) {
+    //score ++ // isme pehle kaam hota hai phir condition check hota hai
+    
+    
+ }
